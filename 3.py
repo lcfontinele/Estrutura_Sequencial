090bcc4a3c4ab278fbc2a468bcbdac70497eb0dc
Lucas Fontinele <76830255+lcfontinele@users.noreply.github.com>
2021-01-21 14:33:02 -0300

    3° Exercício.
    Nesse exercício eu fiz a soma de dois números digitados pelo usuário.

---
+++ 3.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input('Digite o 1° número: '))
+numero2 = int(input('Digite o 2° número: '))
+print(f'A soma entre {numero1} + {numero2} = {numero1+numero2}.')
