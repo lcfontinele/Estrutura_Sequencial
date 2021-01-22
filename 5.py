560c98c74684677b802f5e402f5e9d7325ce50f4
Lucas Fontinele <76830255+lcfontinele@users.noreply.github.com>
2021-01-22 05:29:33 -0300

    5° Exercício.
    Nesse exercício eu fiz um simples conversor de metros em centímetros.

---
+++ 5.py
@@ -0,0 +1,5 @@
+# Faça um Programa que converta metros para centímetros.
+
+metros = int(input('Digite o tamanho em metros(m): '))
+centimetros = metros * 100
+print(f'{metros}(m) em centímetros é igual {centimetros}(cm). ')
